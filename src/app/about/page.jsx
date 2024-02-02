48e8c2c55447052de0e71e7dfaba480f629e5912
ozlem <ozlem.demirkiran3460@gmail.com>
2024-02-02 14:10:35 +0300

    proje oluşturuldu

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+
+
+
+export const metadata = {
+  title: "About Page",
+  description: "About description",
+};
+
+
+export default  function AboutPage (){
+
+  // console.log("lets check where it works")
+  return (
+    <div >
+      
+     merhaba about
+    </div>
+  );
+};
+
+
---
+++ src/app/admin/page.jsx
@@ -0,0 +1,17 @@
+export const metadata = {
+    title: "About Page",
+    description: "About description",
+  };
+  
+  
+  export default  function AdminPage (){
+  
+    // console.log("lets check where it works")
+    return (
+      <div >
+        
+       merhaba admin
+      </div>
+    );
+  };
+  
