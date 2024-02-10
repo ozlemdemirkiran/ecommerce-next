fd19bd7d305503fc2e8be6f08011314c549413ee
ozlemdemirkiran <ozlem.demirkiran.60@hotmail.com>
2024-02-10 20:36:16 +0300

    trending slider yapıldı

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import Popup from './popup';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 
@@ -34,6 +37,14 @@ export default function Trending() {
        setSelectedProduct(findProduct)
     };
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    };
+
     return(
         <>
         {/* style: box-sizing:"border-box" */}
@@ -46,11 +57,10 @@ export default function Trending() {
                 </div>
             </div>
 
-            <div className="row-container" style={{display:"flex", width:"100%", gap:"2.2rem", overflowX:"scroll", overflowY:"hidden", whiteSpace:"nowrap", scrollBehavior:"smooth", position: "relative",
+            {/* <div className="row-container" style={{display:"flex", width:"100%", gap:"2.2rem", overflowX:"scroll", overflowY:"hidden", whiteSpace:"nowrap", scrollBehavior:"smooth", position: "relative",
                 padding: "1rem .6rem"}}>
 
-                {/* card */}
-
+                
                 {trendingData.map((product) => (
                     <div onClick={()=>open(product?.id)} className="flex flex-col gap-2" style={{outline: "2px solid rgba(0, 0, 0, .205)", width:"210px", height:"440px", cursor: "pointer"}}>
                         <div className="item-header  p-2">
@@ -62,8 +72,23 @@ export default function Trending() {
                         </div>
                     </div>
                 ))}
-
             </div>
+             */}
+            <Slider {...settings}>
+                {trendingData.map((product) => (
+                    <div className='trending-slider'>
+                        <div onClick={()=>open(product?.id)} className=" flex flex-col gap-2 cursor-pointer">
+                        <div className="item-header  p-2">
+                            <img src={product.image} alt="product" style={{width: "210px", height:"210px"}} />
+                        </div>
+                        <div className="item-description flex flex-col gap-1 px-2 text-wrap" style={{ width:"210px", height:"auto"}}>
+                            <p style={{fontSize:"1.2rem"}}>{product.title}</p>
+                            <p style={{fontWeight:"bold", fontSize:"1.7rem"}}>{product.price}$</p>
+                        </div>
+                    </div>
+                    </div>
+                ))}
+                </Slider>
 
         </div>
         {selectedProduct ? <Popup  selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct} /> : null}
